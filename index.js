ff1123988c64b8aa1b21d43c5334420a22a97e7d
Nacho Hidalgo <nhidalgotoca@gmail.com>
2022-03-20 20:50:32 +0100

    Update

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const PORT = process.env.PORT || newLocal;
 const uri = process.env.DB_URI;
 client= null;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 var database, collection;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/api/get_courses', function (req, res) {
   collection = database.collection("courses");
@@ -23,6 +28,7 @@ app.get('/api/get_courses', function (req, res) {
     if(error) {
         res.send("Error")
     }
+    console.log(result)
     res.json({
       message: "OK",
       response: result
@@ -30,15 +36,19 @@ app.get('/api/get_courses', function (req, res) {
 });
 });
 
-app.get('/api/login/', function (req, res){
-  var username = req.body.name;
-  var password = req.body.password;
+app.get('/api/login', function (req, res){
+  var username = req.query.username;
+  var password = req.query.password;
   collection = database.collection("users");
-  collection.findOne({"first_name": username,"password":password}, (error,result)=>{
+  collection.findOne({"first_name":username,"password":password}, (error,result)=>{
     if(error){
-
+      res.json({
+        status: "ERROR",
+        message: "Authentication failed",
+        session_token : ""
+      })
     }
-    if(result){
+    if(result) {
       var randNum = Math.floor(Math.random() * (1000 - 1 + 1) + 1);
       var tokenToHash = username+password+randNum;
       var token = crypto.createHash('sha256').update(tokenToHash).digest('hex');
@@ -47,16 +57,54 @@ app.get('/api/login/', function (req, res){
         message: "Correct login, token created",
         session_token : token
       })
+      collection.updateOne({"first_name":username,"password":password},{ $set:{"session_token":token}},function(err,res){
+        if(err) throw err;
+      })
+    }else{
+      res.json({
+        status: "ERROR",
+        query: req.body,
+        message: "Authentication failed",
+        session_token : ""
+      })
+    }
+  });
+});
+
+app.get('/api/logout', function (req, res){
+  var token = req.query.token;
+  collection = database.collection("users");
+  collection.findOne({"session_token":token}, (error,result)=>{
+    if(error){
+      res.json({
+        status: "ERROR",
+        message: "Session Token rquired"
+      })
+    }
+    if(result){
+      if(token == undefined || token == ""){
+        res.json({
+          status: "ERROR",
+          message: "Session Token rquired"
+        })
+        }
+        else{
+          res.json({
+            status: "OK",
+            message: "Session succsesfully closed"
+          })
+      }
     }
     else{
         res.json({
           status: "ERROR",
-          message: "Authentication failed",
-          session_token : token
+          message: "Session Token rquired"
         })
     }
   });
+
 });
+
 
 app.listen(PORT, () => {
   MongoClient.connect(uri, { useNewUrlParser: true, serverApi: ServerApiVersion.v1  }, (error, client) => {
